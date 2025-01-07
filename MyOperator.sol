23ad0e9ed2057c414a288d31b5fde1f77aa7816d
yenachar <yenachar@protonmail.com>
2025-01-07 05:13:26 +0100

    Added code for the operator contract.

---
+++ MyOperator.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.8.0;
+
+import "https://raw.githubusercontent.com/smartcontractkit/chainlink/master/contracts/src/v0.8/operatorforwarder/Operator.sol";
+
+contract MyOperator is Operator {
+    // Define your own minimum gas limit constant
+    uint256 private constant MY_MINIMUM_CONSUMER_GAS_LIMIT = 400000;
+
+    constructor(address link) Operator(link, msg.sender) {}
+
+   function fulfillOracleRequest3(
+       bytes32 requestId,
+       uint256 payment,
+       address callbackAddress,
+       bytes4 callbackFunctionId,
+       uint256 expiration,
+       bytes calldata data
+   )
+       external
+       validateAuthorizedSender
+       validateRequestId(requestId)
+       validateCallbackAddress(callbackAddress)
+       returns (bool)
+   {
+       _verifyOracleRequestAndProcessPayment(requestId, payment, callbackAddress, callbackFunctionId, expiration, 2);
+       emit OracleResponse(requestId);
+       require(gasleft() >= 400000, "Must provide consumer enough gas"); // or your chosen constant
+       (bool success, ) = callbackAddress.call(abi.encodePacked(callbackFunctionId, data));
+       return success;
+   }
+}
+
